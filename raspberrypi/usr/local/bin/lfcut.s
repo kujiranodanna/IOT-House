373240e19e3b25c68ca6e1659579b247a76d6c31
Isamu <70492305+kujiranodanna@users.noreply.github.com>
2020-09-07 05:54:13 +0900

    Update lfcut.s

--- raspberrypi/usr/local/bin/lfcut.s
+++ raspberrypi/usr/local/bin/lfcut.s
@@ -1,4 +1,6 @@
 #!/bin/bash
+# The MIT License
+# Copyright (c) 2020-2027 Isamu.Yamauchi
 # This scripts startup.s ^M cutter.
 # Copyright 2000.11.8 IZAMU.KARERA
 # Usage    startup.s before_txt after_txt
