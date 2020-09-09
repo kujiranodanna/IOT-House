71b0f8be10e9468965bbf6c3835cdd3d736b3774
Isamu <70492305+kujiranodanna@users.noreply.github.com>
2020-09-10 06:08:40 +0900

    Update term.sh

--- raspberrypi/www/pepolinux/term.sh
+++ raspberrypi/www/pepolinux/term.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # The MIT License
-# Copyright (c) 2020-2027 Isamu.Yamauchi , very old update 2020.9.10
+# Copyright (c) 2020-2027 Isamu.Yamauchi , 2020.9.10 update 2020.9.10
 epicon -q -d 30 -D 50 -l /dev/ttyS1 
